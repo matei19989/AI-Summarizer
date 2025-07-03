0a47d8f6becc902b7bf1fd31e36b249f689ba8d8
matei1998 <mateiserband@gmail.com>
2025-07-03 18:11:43 +0300

    ceva

--- AISummarizerAPI/obj/Debug/net9.0/AISummarizerAPI.AssemblyInfo.cs
+++ AISummarizerAPI/obj/Debug/net9.0/AISummarizerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AISummarizerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd9229ae7fcd4e0587dfbad24a688281bc17764e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f78787c73f314cbf4aa9da0a079dc8e6f66e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AISummarizerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AISummarizerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
