eb2a85894db3bf5237aedf9c57c15231f9c978e3
matei1998 <mateiserband@gmail.com>
2025-07-06 15:26:41 +0300

    docker doesn't work

--- AISummarizerAPI/obj/Debug/net9.0/AISummarizerAPI.AssemblyInfo.cs
+++ AISummarizerAPI/obj/Debug/net9.0/AISummarizerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AISummarizerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11f42bcc52fb2cbc0f0b47438e3727080c1b820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f311716c0b49fbdc6c4695a6357bc42e19eb235")]
 [assembly: System.Reflection.AssemblyProductAttribute("AISummarizerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AISummarizerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
